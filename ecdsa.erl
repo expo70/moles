ec01034a5ee564864bf6bc0756d33ad00883eda6
Kanso PhD <expo70.xyz@gmail.com>
2017-10-29 00:00:36 +0900

    added signature and compressed/uncompressed public key parsings

--- ecdsa.erl
+++ ecdsa.erl
@@ -185,6 +185,111 @@ parse_signature_DER(<<16#30, CompoundLen:8, Rest:CompoundLen/binary>>) ->
 
 	{R,S}.
 
+%% ref: https://bitcoin.org/en/developer-guide#public-key-formats
+%% uncompressed format - 0x04 <X> <Y>
+%% compressed format -   0x02 <X> when Y is even num.
+%%                       0x03 <X> when Y is odd num.
+parse_public_key(<<16#04, X:256/big, Y:256/big>>) -> {X,Y};
+parse_public_key(<<16#02, X:256/big>>) ->
+	{Y1,Y2} = qy_from_qx(X),
+	case Y1 rem 2 of
+		0 -> {X,Y1};
+		1 -> {X,Y2}
+	end;
+parse_public_key(<<16#03, X:256/big>>) ->
+	{Y1,Y2} = qy_from_qx(X),
+	case Y1 rem 2 of
+		0 -> {X,Y2};
+		1 -> {X,Y1}
+	end.
+
+
+
+%% obtain Qy of public key from Qx
+%% E: y^2 == x^3 + 7 mod P
+qy_from_qx(X) ->
+	sqrt_mod(mod(mod(X*X,?P)*X+7,?P), ?P).
+
+
+%% Euler's criterion
+%% P - odd prime
+%% evaluate N^((P-1)/2) mod P
+euler_criterion(N, P) ->
+	Eu = modpow(N, (P-1) div 2, P),
+	if
+		Eu == P-1 -> -1;
+		true -> Eu
+	end.
+
+%% N = Q 2^S
+obtain_Q_2S(N) -> obtain_Q_2S(N, 0).
+
+obtain_Q_2S(Q, S) ->
+	case Q rem 2 of
+		0 -> obtain_Q_2S(Q div 2, S+1);
+		1 -> {Q, S}
+	end.
+
+%% P - odd prime
+find_quadratic_non_residue(P) -> find_quadratic_non_residue(2, P).
+
+find_quadratic_non_residue(Z, P) when Z < P ->
+	case euler_criterion(Z, P) of
+		-1 -> Z;
+		 1 -> find_quadratic_non_residue(Z+1, P)
+	end.
+
+%% T^(2^I) == 1 mod P
+find_2pI(T, P) when T/=0 -> find_2pI({T, 0}, T, P).
+
+find_2pI({Acc, I}, T, P) ->
+	case mod(Acc, P) of
+		1 -> I;
+		_ -> find_2pI({mod(Acc*Acc,P), I+1}, T, P)
+	end.
+
+%% 2^N
+pow2(N) when is_integer(N) andalso N>=0 -> pow2(1, N).
+
+pow2(Acc, 0) -> Acc;
+pow2(Acc, N) -> pow2(2*Acc, N-1).
+
+%% obtaining modular square root of n
+%% by using Tonelli-Shanks algorithm
+%% ref: https://en.wikipedia.org/wiki/Tonelli%E2%80%93Shanks_algorithm
+%% P - odd prime
+sqrt_mod(N, P) ->
+	case euler_criterion(N, P) of
+		 0 -> 0;
+		-1 -> throw(is_a_quadratic_non_residue);
+		 1 ->
+			{Q, S} = obtain_Q_2S(P-1),
+		 	Z = find_quadratic_non_residue(P),
+			R = sqrt_mod_loop(
+				{
+					S, 
+					modpow(Z, Q, P),
+					modpow(N, Q, P),
+					modpow(N, (Q+1) div 2, P)
+				}, P),
+			{R, P-R}
+	end.
+
+sqrt_mod_loop({M, C, T, R}, P) ->
+	case T of
+		1 -> R;
+		_ ->
+			I = find_2pI(T, P),
+			B = modpow(C, pow2(M-I-1), P),
+			sqrt_mod_loop(
+			{
+				I,
+				mod(B*B, P),
+				mod(T*B*B, P),
+				mod(R*B, P)
+			}, P)
+	end.
+
 
 %% Modular
 mod(A, M) when is_integer(A), is_integer(M), A>=0, M>0 ->
@@ -208,6 +313,22 @@ inverse_mod(A, M) when is_integer(A), is_integer(M), A>0, M>0 ->
 		1 -> mod(InvMod, M); % for possible negative value
 		_ -> throw(no_inverse_mod)
 	end.
+
+%% N^P mod M
+%% by using binary method
+%% 
+%% Erlang's crypto:mod_pow() returnes binary
+%% <<R/big>> = crypto:mod_pow(N, P, M), % doesn't work for large integers
+modpow(N, P, M) when is_integer(P) andalso P>0 -> modpow(1, N, P, M).
+
+modpow(Acc, _, 0, _) -> Acc;
+modpow(Acc, N, P, M) ->
+	{Acc1, N1} = case P rem 2 of
+		0 -> {Acc,           mod(N*N, M)};
+		1 -> {mod(Acc*N, M), mod(N*N, M)}
+	end,
+	modpow(Acc1, N1, P div 2, M).
+
 
 
 -ifdef(EUNIT).
